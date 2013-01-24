8f0deee6f04a37a0c03ad0bb5677e0c052f22fe6
Jonathan Baudanza <jon@jonb.org>
2013-01-24 14:59:59 -0800

    Updating post-receive hook

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Pick an AMI. http://uec-images.ubuntu.com/releases/quantal/release/
 Startup user-data
 
     #!/bin/sh
-    curl https://gist.github.com/raw/4515888/9632d675d2625bc348539307b570644a2654af76/user-data.sh | bash -ex
+    curl https://gist.github.com/raw/4515888/75f207ae31ac841bbb0c25fffd9204380938f4dd/user-data.sh | bash -ex
 
 Checkout ruby buildpacks
 
@@ -17,9 +17,16 @@ Checkout ruby buildpacks
 
 Create git repos on server
 
-    git init --bare myproject
+    git init --bare repos/myproject
+
+Setting the postgres password
+
+    psql postgres
+    ALTER USER app WITH PASSWORD 'app';
 
 Link the post-receive hook
+
+    ln -s ~/slugs/bluevan/post-receive.rb repos/myproject/hooks/post-receive
 
 Locally
 
@@ -31,4 +38,34 @@ Locally
 
 Generate upstart scripts
 
-    sudo foreman export --user=app --root=~app/slugs/bluevan upstart /etc/init
+    sudo foreman export --app myproject --port 6000 --user=app --root=~app/slugs/myproject upstart /etc/init
+
+    sudo start myproject
+
+Start nginx
+
+    sudo service nginx start
+
+Sample nginx config
+
+    upstream jonborg {
+      server localhost:6000;
+    }
+
+    proxy_set_header  X-Real-IP  $remote_addr;
+    proxy_set_header  X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header  Host $http_host;
+
+    proxy_redirect off;
+
+    server {
+      server_name jonb.org www.jonb.org;
+
+      root /home/app/slugs/jonborg/public/;
+
+      location / {
+        if (!-f $request_filename) {
+          proxy_pass http://jonborg;
+        }
+      }
+    }
--- post-receive.rb
+++ post-receive.rb
@@ -1,23 +1,39 @@
 #!/usr/bin/env ruby
-puts ARGV.inspect
+require 'yaml'
 
-# ln -s /home/app/slugs/bluevan/post-receive.rb /home/app/repos/bluevan/hooks/post-receive
+REPO_NAME = Dir.pwd.split('/').last.freeze
+HOME_DIR = '/home/app'.freeze
+BUILDPACK_PARENT_DIR = "#{HOME_DIR}/buildpacks/".freeze
+RELEASE_DIR = "#{HOME_DIR}/slugs/#{REPO_NAME}".freeze
+CACHE_DIR = "#{HOME_DIR}/cache/#{REPO_NAME}".freeze
 
-HOME_DIR = '/home/app'
-BUILDPACK_DIR = "#{HOME_DIR}/buildpacks/heroku-buildpack-ruby".freeze
-RELEASE_DIR = "#{HOME_DIR}/slugs/bluevan"
-CACHE_DIR = "#{HOME_DIR}/cache/"
-
-puts "Checking out master"
+Dir.mkdir(RELEASE_DIR) unless File.exists?(RELEASE_DIR)
+Dir.mkdir(CACHE_DIR) unless File.exists?(CACHE_DIR)
 
 system({'GIT_WORK_TREE' => RELEASE_DIR}, 'git reset --hard master')
 
-# TODO: slurp the output of this script
-if system("#{BUILDPACK_DIR}/bin/detect", RELEASE_DIR)
-  system("#{BUILDPACK_DIR}/bin/compile", RELEASE_DIR, CACHE_DIR)
+environment = {
+  'RAILS_ENV' => 'production',
+  'RACK_ENV' => 'production',
+  'DATABASE_URL' => "postgres://app@app:localhost/#{REPO_NAME}"
+}
 
-  # TODO: Run the /bin/release script to setup a Procfile
-else
-  puts "ERROR: No suitable buildback found"
+Dir.glob("#{BUILDPACK_PARENT_DIR}/*").each do |buidpack_dir|
+  slug_type = `#{buidpack_dir}/bin/detect #{RELEASE_DIR}`
+  if $?.success?
+    puts "Detected #{slug_type.chomp}"
+
+    system("#{buidpack_dir}/bin/compile", RELEASE_DIR, CACHE_DIR)
+
+    yaml = `#{buidpack_dir}/bin/release #{RELEASE_DIR}`
+    parsed = YAML.parse(yaml)
+    #puts parsed['default_process_types'].inspect
+
+    # Create environment file
+    File.open("#{RELEASE_DIR}/.env", 'w') do |file|
+      environment.each do |key, value|
+        file.puts "#{key}=#{value}"
+      end
+    end
+  end
 end
-
